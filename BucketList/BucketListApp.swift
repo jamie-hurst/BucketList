fa4f00c93ec3467fe9e21a7efbb965295c30b452
warmrome <pxpx786@gmail.com>
2022-01-06 11:26:13 -0800

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Jameson Hurst on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
