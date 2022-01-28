cf5048a27527ef4fe9df1aab95d5379ab45820c5
warmrome <pxpx786@gmail.com>
2022-01-28 11:33:32 -0800

    initial commit

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -2,15 +2,75 @@
 //  ContentView.swift
 //  BucketList
 //
-//  Created by Jameson Hurst on 1/6/22.
+//  Created by Jameson Hurst on 1/16/22.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = ViewModel()
+    
+    
     var body: some View {
-        Text("Hello, world!")
+        if viewModel.isUnlocked {
+            ZStack {
+                Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations) { location in
+                    MapAnnotation(coordinate: location.coordinate) {
+                        VStack {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundColor(.red)
+                                .frame(width: 44, height: 44)
+                                .background(.white)
+                                .clipShape(Circle())
+                            
+                            Text(location.name)
+                                .fixedSize()
+                        }
+                        .onTapGesture {
+                            viewModel.selectedPlace = location
+                        }
+                    }
+                }
+                .ignoresSafeArea()
+                Circle()
+                    .fill(.blue)
+                    .opacity(0.3)
+                    .frame(width: 32, height: 32)
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button {
+                            viewModel.addLocation()
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                        .padding()
+                        .background(.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
+                    }
+                }
+            }
+            .sheet(item: $viewModel.selectedPlace) { place in
+                EditView(location: place) {
+                    viewModel.update(location: $0)
+                }
+            }
+            
+        } else {
+            Button("Unlock Places") {
+                viewModel.authenticate()
+            }
             .padding()
+            .background(.blue)
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+        }
     }
 }
 
